92eabbe1dbbfc8969e8d21eff3083850d8d2ef3c
Emily Lemmon <eclaire.lemmon@gmail.com>
2019-01-02 13:53:41 -0600

    last steps

--- orderTots.js
+++ orderTots.js
@@ -42,3 +42,27 @@ businesses.forEach(business => {
 
 
 
+
+
+
+
+
+
+
+
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10];
+
+const totalRainfall = monthlyRainfall.reduce((currentTotal, upcoming) => 
+     currentTotal + upcoming
+     ,0)
+
+console.log(totalRainfall);
+
+
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+const sentence = words.reduce((currentTotal, nextValue)=> 
+currentTotal += ` ${nextValue}`)
+
+console.log(sentence)
+
